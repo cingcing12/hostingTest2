d7da03fc7e67b2645bffc91eee2d6175f4b939d3
Vong Sokpheak <vongsokpheak@example.com>
2025-09-22 18:08:50 +0700

    Updated app.js with new feature/bug fix

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors());
 const port = process.env.port || 3000;
+
 
 app.get("/", (req, res) => {
     res.send("Successfully!");
